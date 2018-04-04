cc62bd1e81cc49c7e7c3f0759bbae6bc27d71a69
Pyreandice <33629589+Pyreandice@users.noreply.github.com>
2018-04-04 15:22:58 -0500

    Autorun and Analytics added
    Dont forget to enter your google analytics info next time

---
+++ Javascript/autorun.js
@@ -0,0 +1,7 @@
+   function autorun()
+   {
+    
+   }
+   if (window.addEventListener) window.addEventListener("load", autorun, false);
+   else if (window.attachEvent) window.attachEvent("onload", autorun);
+   else window.onload = autorun;
