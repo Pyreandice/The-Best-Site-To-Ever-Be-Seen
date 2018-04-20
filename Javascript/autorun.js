3ff543648cf8614a847e0b126b87d332f4f94032
Pyreandice <33629589+Pyreandice@users.noreply.github.com>
2018-04-20 11:58:06 -0500

    Not much change

--- Javascript/autorun.js
+++ Javascript/autorun.js
@@ -1,7 +1,9 @@
    function autorun()
    {
-    
+    if (window.addEventListener) window.addEventListener("load", autorun, false);
+    else if (window.attachEvent) window.attachEvent("onload", autorun);
+    else window.onload = autorun;
    }
-   if (window.addEventListener) window.addEventListener("load", autorun, false);
-   else if (window.attachEvent) window.attachEvent("onload", autorun);
-   else window.onload = autorun;
+//   if (window.addEventListener) window.addEventListener("load", autorun, false);
+// else if (window.attachEvent) window.attachEvent("onload", autorun);
+//else window.onload = autorun;
