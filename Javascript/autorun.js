3c384553424c6490de5ef690363e4efec9a9ad90
grayson.beam <graysonbeam@gmail.com>
2018-06-06 11:39:57 -0500

    More minor changes

--- Javascript/autorun.js
+++ Javascript/autorun.js
@@ -1,11 +1,5 @@
    function autorun()
    {
-<<<<<<< HEAD
-       if (window.addEventListener) window.addEventListener("load", autorun, false);
-       else if (window.attachEvent) window.attachEvent("onload", autorun);
-       else window.onload = autorun;
-   };
-=======
     if (window.addEventListener) window.addEventListener("load", autorun, false);
     else if (window.attachEvent) window.attachEvent("onload", autorun);
     else window.onload = autorun;
@@ -13,4 +7,3 @@
 //   if (window.addEventListener) window.addEventListener("load", autorun, false);
 // else if (window.attachEvent) window.attachEvent("onload", autorun);
 //else window.onload = autorun;
->>>>>>> 5bb78f4273ff972c2509be6d171ec2a8713dbe02
